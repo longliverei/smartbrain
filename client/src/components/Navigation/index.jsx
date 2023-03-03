5484b9a7dfef2d1a6f020bc869b807b72a7fcb22
Reinaldo <reinaldoybk@hotmail.com>
2023-03-03 20:25:21 -0300

    clean files and Logo Component

---
+++ client/src/components/Navigation/index.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import './style.scss';
+
+export const Navigation = () => {
+    return (
+        <nav className="nav">
+            <p className="sign">Sign Out</p>
+        </nav>
+    )
+}
